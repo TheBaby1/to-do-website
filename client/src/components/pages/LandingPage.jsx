f0e56b4861a4d2028fa9bb5d55d0c97c60af9539
Nikko Ensomo <nikkonuriel@gmail.com>
2026-03-08 02:33:53 +0800

    Added lading page texts

--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -5,8 +5,15 @@ const LandingPage = () => {
         <>
             <MainHeader></MainHeader>
 
-            <div className="w-full max-w-screen-xl mx-auto px-4 flex flex-col">
-        
+            <div className="w-full max-w-screen-xl mx-auto px-4 flex flex-row overflow-y-auto mt-[28px]">
+                <div className="flex flex-col mx-auto -space-y-[24px] mt-[32px]">
+                    <h1 className="font-extrabold text-[56px]">COMPLETE</h1>
+                    <h1 className="font-extrabold text-[56px]">YOUR</h1>
+                    <h1 className="font-extrabold text-[56px]">TASKS</h1>
+
+                    
+                </div>
+
                 
             </div>
         </>
