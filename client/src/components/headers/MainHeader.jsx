777f4b01dd125e9f33ec23af78c8b6065515d68a
Nikko Ensomo <nikkonuriel@gmail.com>
2026-03-11 03:28:23 +0800

    Added cursor pointer in navigation options

--- client/src/components/headers/MainHeader.jsx
+++ client/src/components/headers/MainHeader.jsx
@@ -6,9 +6,9 @@ const MainHeader = () => {
             <div className="flex flex-row justify-between bg-white px-[24px] py-[24px] border-b border-gray-100 mx-auto">
                 <h1>To Do List</h1>
                 <div className="flex flex-row gap-4">
-                    <h1>Home</h1>
-                    <h1>Lists</h1>
-                    <h1>Sign In</h1>
+                    <h1 className="cursor-pointer">Home</h1>
+                    <h1 className="cursor-pointer">Lists</h1>
+                    <h1 className="cursor-pointer">Sign In</h1>
                 </div>
             </div>
         </>
