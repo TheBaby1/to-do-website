e78b607d467f15b6a8d3de5ff35fdd02f564450c
Nikko Ensomo <nikkonuriel@gmail.com>
2026-03-11 03:49:27 +0800

    Added button jsx

---
+++ client/src/components/buttons/CreateListButton.jsx
@@ -0,0 +1,13 @@
+const CreateListButton = () => {
+    return (
+        <>
+            <button
+                className="h-full w-max-[200px] bg-black text-white font-semibold px-6 py-2 rounded-lg cursor-pointer shadow-md"
+            >
+                CREATE YOUR LIST
+            </button>
+        </>
+    );
+}
+
+export default CreateListButton;
