51cb1b1aac9813ed6e46fca43f5b7408c03e37e8
Nikko Ensomo <nikkonuriel@gmail.com>
2026-03-11 03:58:14 +0800

    Added button to landing page and fixed spacing

--- client/src/components/buttons/CreateListButton.jsx
+++ client/src/components/buttons/CreateListButton.jsx
@@ -2,7 +2,7 @@ const CreateListButton = () => {
     return (
         <>
             <button
-                className="h-full w-max-[200px] bg-black text-white font-semibold px-6 py-2 rounded-lg cursor-pointer shadow-md"
+                className="self-start bg-black text-white font-semibold px-6 py-2 rounded-lg cursor-pointer shadow-md"
             >
                 CREATE YOUR LIST
             </button>
--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -1,4 +1,6 @@
 import MainHeader from "../headers/MainHeader";
+import CreateListButton from "../buttons/CreateListButton";
+
 import LandingImage from "../../assets/images/landing_page_logo.png"
 import LandingImageLogo from "../../assets/images/landing_logo.jpg"
 
@@ -8,10 +10,12 @@ const LandingPage = () => {
             <MainHeader></MainHeader>
 
             <div className="w-full max-w-screen-xl mx-auto px-4 flex flex-row overflow-y-auto mt-[28px]">
-                <div className="flex flex-col mx-auto -space-y-[24px] mt-[32px]">
-                    <h1 className="font-extrabold text-[56px]">COMPLETE</h1>
-                    <h1 className="font-extrabold text-[56px]">YOUR</h1>
-                    <h1 className="font-extrabold text-[56px]">TASKS</h1>
+                <div className="flex flex-col mx-auto mt-[32px]">
+                    <div className="-space-y-[24px]">
+                        <h1 className="font-extrabold text-[56px]">COMPLETE</h1>
+                        <h1 className="font-extrabold text-[56px]">YOUR</h1>
+                        <h1 className="font-extrabold text-[56px]">TASKS</h1>
+                    </div>
 
                     <p className="text-gray-500 mt-[24px]"
                     >
@@ -20,11 +24,13 @@ const LandingPage = () => {
                         Whether you're planning your work, managing personal goals, or tracking daily habits,
                         everything you need is in one simple place.
                     </p>
+
+                    <CreateListButton></CreateListButton>
                 </div>
 
                 <div className="flex items-center justify-center">
                     <img
-                        className="w-full max-2-[600px] h-auto object-cover"
+                        className="w-full max-w-[600px] h-auto object-cover"
                         src={LandingImageLogo}
                         alt="LandingImage"
                     >
